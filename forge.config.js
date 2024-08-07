5b332a12cf64e9a222dd75aad56aca157fa9d1a7
HB9HIL <fabian.berg@hb9hil.org>
2024-08-07 20:32:58 +0200

    adjustments

--- forge.config.js
+++ forge.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			name: '@electron-forge/publisher-github',
 			config: {
 				repository: {
-					owner: 'wavelog',
+					owner: 'HB9HIL',
 					name: 'WaveLogGate'
 				},
 				prerelease: false
